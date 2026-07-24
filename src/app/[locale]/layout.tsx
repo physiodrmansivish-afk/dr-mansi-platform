7b8a843900c4270a9b079d81562d7e6a8bf7e207
physiodrmansivish-afk <physiodrmansivish@gmail.com>
2026-07-24 15:21:22 +0530

    chore: added Vercel Speed Insights component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,7 +7,7 @@ import Footer from '@/components/shared/Footer';
 
 import { getTranslations } from 'next-intl/server';
 import { Analytics } from "@vercel/analytics/next";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 type LocaleLayoutProps = {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
@@ -103,6 +103,7 @@ export default async function LocaleLayout({
           <Footer />
         </NextIntlClientProvider>
         <Analytics />
+        <SpeedInsights />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
