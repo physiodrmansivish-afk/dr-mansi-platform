13bdf6c52ca04b360e366a7de9b289cd29746366
physiodrmansivish-afk <physiodrmansivish@gmail.com>
2026-07-25 19:26:37 +0530

    Capitalize brand name to Physiomansi

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -44,7 +44,7 @@ export default function Navbar() {
           href={`/${locale}`}
           className="text-xl font-bold text-primary"
         >
-          physiomansi
+          Physiomansi
         </Link>
 
         {/* Desktop Navigation */}
